ed5edc64f65471ee4688c09afb00019995f62a9a
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-07 20:55:06 +0530

    Prisma commit

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = 3000
 
 app.use(express.json())
-
-app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/', (req, res) => {
     console.log(req.body)
     res.json({ message: "Prisma ORM" })
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:3000`))
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:3000`)
+});
